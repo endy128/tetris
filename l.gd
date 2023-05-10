73b1f6f2a77e855d448ee069c4036660a67031bc
Neil Douglas <endy64@icloud.com>
2023-05-10 11:51:29 +0100

    initial commit

---
+++ l.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+var move_vector = Vector2.ZERO
+var move_distance = 5000
+var is_active = true
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print(global_position.x)		
+	# FIXME: needs to add in the size of the blocks
+	if ( global_position.x > -166 and global_position.x < 160):
+		if(Input.is_action_just_pressed("left")):
+			_move(-move_distance)
+		if(Input.is_action_just_pressed("right")):
+			_move(move_distance)
+	if(Input.is_action_just_pressed("clockwise")):
+		_rotate(90)
+	if(Input.is_action_just_pressed("anti_clockwise")):
+		_rotate(-90)
+	
+func _move(direction):
+	move_vector.x = direction
+	set_velocity(move_vector)
+	move_and_slide()
+
+func _rotate(degree):
+	rotation_degrees += degree
+
+func drop(drop_vector):
+	if(Input.is_action_pressed("down")):
+		set_velocity(drop_vector * 10)
+	else:	
+		set_velocity(drop_vector)
+	if global_position.y < 200:
+		move_and_slide()
+
+func has_landed():
+	is_active = false
---
+++ main.gd
@@ -0,0 +1,33 @@
+extends Node
+
+var score = 0
+var move_speed = 150
+var acceleration = 2
+var drop_speed = 50
+var drop_vector = Vector2(0, drop_speed)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	move_speed += acceleration * delta
+	get_node("l").drop(drop_vector)
+	#drop_vector += Vector2(0, drop_speed)
+
+func _spawn_shape():
+	pass
+	
+func _check_win_lose():
+	pass
+	
+func _check_lines():
+	pass
+	
+func _del_line():
+	pass
+	
+func _drop_blocks():
+	pass
