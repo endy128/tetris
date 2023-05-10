04504335e1a6cd9cc3d558ed3041a2c8ef0f542d
Neil Douglas <endy64@icloud.com>
2023-05-10 13:36:51 +0100

    grid drawn in code, and can relocate

--- Node2D.gd
+++ Node2D.gd
@@ -2,10 +2,13 @@ extends Node2D
 
 const COLUMNS = 10
 const ROWS = 20
-const BLOCK_SIZE = 29
+const BLOCK_SIZE = 30
 const GRID_WIDTH = 1
-const START_X = 10
-const START_Y = 10
+const START_X = 20
+const START_Y = 30
+const GRID_BG = "#000000"
+const GRID_COLOUR = "#FFFFFF"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,9 +21,12 @@ func _process(delta):
 
 
 func _draw():
-	draw_rect( Rect2(START_X, START_Y, (COLUMNS * BLOCK_SIZE + (10 * GRID_WIDTH)) - START_X, (ROWS * BLOCK_SIZE + (10 * GRID_WIDTH)) - START_Y), "#0000FF", true, 1)
-	for i in range(START_X, ((BLOCK_SIZE + GRID_WIDTH ) * COLUMNS) + GRID_WIDTH, BLOCK_SIZE):
-		draw_line( Vector2(i ,START_Y), Vector2( i, ROWS * BLOCK_SIZE + (COLUMNS * GRID_WIDTH)), "#FFFFFF", 1)
-	for i in range(START_Y, 611, BLOCK_SIZE):
-		draw_line(Vector2(START_X, i), Vector2(COLUMNS * BLOCK_SIZE + (COLUMNS * GRID_WIDTH), i ), "#FFFFFF", 1)
+	draw_rect( Rect2(START_X, START_Y, COLUMNS * BLOCK_SIZE, ROWS * BLOCK_SIZE), GRID_BG, true, GRID_WIDTH)
+	# draw the column gridlines
+	for i in range(START_X, BLOCK_SIZE * COLUMNS + START_X + 1, BLOCK_SIZE):
+		draw_line( Vector2(i , START_Y), Vector2( i, ROWS * BLOCK_SIZE + START_Y), GRID_COLOUR, GRID_WIDTH)
+	
+	# draw the row gridlines
+	for i in range(START_Y, BLOCK_SIZE * ROWS + START_Y + 1, BLOCK_SIZE):
+		draw_line(Vector2(START_X, i), Vector2(COLUMNS * BLOCK_SIZE + START_X, i ), GRID_COLOUR, GRID_WIDTH)
 	
